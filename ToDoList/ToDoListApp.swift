3f0e8254b1d16f2367abb4694a469812b39228fb
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-05-12 21:49:32 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ikbal Demirdoven on 2023-05-12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
